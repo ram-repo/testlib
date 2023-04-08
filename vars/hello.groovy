018445617cd5000db4b9fcec6b2fbd94c104ee98
ram-repo <ramjagadeesh2205@gmail.com>
2023-04-08 15:09:24 +0530

    Create hello.groovy

---
+++ vars/hello.groovy
@@ -0,0 +1,14 @@
+
+def call() {
+  pipeline {
+      agent any
+
+      stages {
+          stage('Hello') {
+              steps {
+                  echo 'Hello World'
+              }
+          }
+      }
+  }
+}
